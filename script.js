ac5972de9d94cc84d952d3849b1c80bee139e18f
grasielarosa <grasielarosaa@gmail.com>
2021-04-23 00:13:17 +0200

    ejercicios 1 a 4 y 9

---
+++ script.js
@@ -0,0 +1,46 @@
+// EJERCÍCIO 1
+// const nombre = prompt('Ingrese un nombre');
+// const saludo=document.querySelector('h1')
+// saludo.innerHTML = (`Hola ${nombre}!`);
+// saludo.style.fontSize = '45px';
+// saludo.style.textAlign = 'center';
+// saludo.style.fontFamily = 'Courier New';
+// saludo.style.color = 'red';
+
+//EJERCICIO 2
+// const color = prompt('Ingrese un color');
+// const cambio=document.querySelector('body')
+// cambio.style.background = color
+
+//EJERCICIO 3
+// const rojo = prompt('Ingrese un numero entre 0 y 255');
+// const verde = prompt('Ingrese otro numero entre 0 y 255');
+// const azul = prompt('Ingrese un ultimo numero entre 0 y 255');
+// const cambio=document.querySelector('body')
+// cambio.style.background = `rgb(${rojo}, ${verde}, ${azul})`
+
+//EJERCICIO 4
+// const tamano = prompt('Quieres la imagen chica, mediana o grande?')
+// const image = document.querySelector('img')
+// if (tamano=='chica'){
+//     image.style.width = '200px'
+// }
+// else if (tamano=='mediana'){
+//     image.style.width = '500px'
+// }
+// else if (tamano=='grande'){
+//     image.style.width = '800px'
+// }
+
+//EJERCÍCIO 9
+// const animal = prompt('Animal gato, perro o loro')
+// const image = document.querySelector('img')
+// if (animal=='gato'){
+//     image.src='https://ichef.bbci.co.uk/news/640/cpsprodpb/10E9B/production/_109757296_gettyimages-1128004359.jpg'
+// }
+// else if(animal=='perro'){
+//     image.src='http://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg'
+// }
+// else if (animal=='loro'){
+//     image.src='https://upload.wikimedia.org/wikipedia/commons/d/dd/Scarlet_Macaw_and_Blue-and-gold_Macaw.jpg'
+// }
